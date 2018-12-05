3321f8e7a5fb9d7aadc43c3d993a4b795ea7bde2
alecamaracm <alecamar@hotmail.es>
2018-12-05 13:03:30 -0500

    ys

--- RealGame/RealGame.v
+++ RealGame/RealGame.v
@@ -26,7 +26,6 @@ wire [10:0]mouseY;
 wire [3:0]wasd;
 wire [3:0]arrows;
 
-wire [10:0]pointY;
 
 output TX;
 
@@ -40,12 +39,57 @@ output hsync,vsync;
 wire [7:0]rrr;
 wire u1;
 
+wire [9:0]BACK2X;
+wire [9:0]BACK2Y;
+wire [9:0]BACK1X;
+wire [9:0]BACK1Y;
+wire [9:0]PIPEDOWN1X;
+wire [9:0]PIPEDOWN1Y;
+wire [9:0]PIPEDOWN2X;
+wire [9:0]PIPEDOWN2Y;
+wire [9:0]PIPEDOWN3X;
+wire [9:0]PIPEDOWN3Y;
+wire [9:0]pointY;
+wire [9:0]pointX;
+wire [9:0]PIPEUP1X;
+wire [9:0]PIPEUP1Y;
+wire [9:0]PIPEUP2X;
+wire [9:0]PIPEUP2Y;
+wire [9:0]PIPEUP3X;
+wire [9:0]PIPEUP3Y;
+wire PIPEDOWN1VISIBLE;
+wire PIPEDOWN2VISIBLE;
+wire PIPEDOWN3VISIBLE;
+wire [9:0]PIPEUP1SKIPY;
+wire PIPEUP1VISIBLE;
+wire [9:0]PIPEUP2SKIPY;
+wire PIPEUP2VISIBLE;
+wire [9:0]PIPEUP3SKIPY;
+wire PIPEUP3VISIBLE;
+wire [9:0]cloud1X;
+wire [9:0]cloud1Y;
+wire [9:0]cloud2X;
+wire [9:0]cloud2Y;
+wire [9:0]cloud3X;
+wire [9:0]cloud3Y;
+
+	assign pointX=550;
+	assign cloud1X=180;
+	assign cloud1Y=70;
+	assign cloud2X=290;
+	assign cloud2Y=90;
+	assign cloud3X=435;
+	assign cloud3Y=60;
+	assign PIPEDOWN1X=250;
+   assign PIPEDOWN1Y=pointY;
+	assign PIPEDOWN1VISIBLE=1;
+
 UART2 uart(CLOCK,RX,key[1],rrr,u1,8'd0,1'b0);
 
 VGADriver driver(CLOCK,hsync,vsync,VGAclock,VGAblanck,VGAsync,xPixel,yPixel,animationClOCK);
 
 
-PP2VerilogDrawingController drawings(CLOCK,key[2],animationClOCK,wasd,arrows,xPixel,yPixel,VGAr,VGAg,VGAb,mouseX,mouseY,580,pointY); //This file is generated automatically by the compiler. Please DO NOT modify it.
+PP2VerilogDrawingController drawings(CLOCK,reset,animationCLOCK,wasd,arrows,xPixel,yPixel,VGAr,VGAg,VGAb,mouseX,mouseY,BACK2X,BACK2Y,BACK1X,BACK1Y,PIPEDOWN1X,PIPEDOWN1Y,PIPEDOWN2X,PIPEDOWN2Y,PIPEDOWN3X,PIPEDOWN3Y,pointX,pointY,PIPEUP1X,PIPEUP1Y,PIPEUP2X,PIPEUP2Y,PIPEUP3X,PIPEUP3Y,cloud1X,cloud1Y,cloud2X,cloud2Y,cloud3X,cloud3Y,PIPEDOWN1VISIBLE,PIPEDOWN2VISIBLE,PIPEDOWN3VISIBLE,PIPEUP1SKIPY,PIPEUP1VISIBLE,PIPEUP2SKIPY,PIPEUP2VISIBLE,PIPEUP3SKIPY,PIPEUP3VISIBLE);
 
 ps2Keyboard keyboard(CLOCK,ps2ck2,ps2dt2,wasd,arrows,leds[4],leds[5]);//
 ps2Mouse mouse(CLOCK,ps2ck,ps2dt,leds[0],leds[1],!key[0],mouseX,mouseY);//;
